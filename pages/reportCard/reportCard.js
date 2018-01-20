a8bed1584a487b52fef2862e6da9031039ba8b81
debrian <debrianfigueroa@gmail.com>
2018-01-20 01:30:44 -0500

    basic setup for report card page is done

--- pages/reportCard/reportCard.js
+++ pages/reportCard/reportCard.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
+  var mentalHealthBtn = document.getElementById('mentalHealthBtn');
 
+  mentalHealthBtn.addEventListener('click', function(){
+    window.location.href = "../mentalHealth/mentalHealth.html";
+  });
+
+  var nutritionBtn = document.getElementById('nutritionBtn');
+
+  nutritionBtn.addEventListener('click', function(){
+    window.location.href = "../nutrition/nutrition.html";
+  });
+
+  var fitnessBtn = document.getElementById('fitnessBtn');
+
+  fitnessBtn.addEventListener('click', function(){
+    window.location.href = "../fitness/fitness.html";
+  });
+
+  var financesBtn = document.getElementById('financesBtn');
+
+  financesBtn.addEventListener('click', function(){
+    window.location.href = "../finances/finances.html";
+  });
+
+  var socialCircleBtn = document.getElementById('socialCircleBtn');
+
+  socialCircleBtn.addEventListener('click', function(){
+    window.location.href = "../socialCircle/socialCircle.html";
+  });
 });
