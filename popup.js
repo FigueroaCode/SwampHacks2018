9583644e6e696029c61373baa6067667c5a4af4e
Luis Garcia <garcialuis1998@yahoo.es>
2018-01-20 01:30:26 -0500

    Working on the initial page.

--- popup.js
+++ popup.js
@@ -1,9 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+
   var nextBtn = document.getElementById("nextBtn");
+  var nameInput = document.getElementById("nameInput");
+  var studentCheckBox = document.getElementById("studentCheckBox");
+  var checkUpVal = document.getElementById("checkUpVal");
+  var leftArrowBtn = document.getElementById("leftArrowBtn");
+  var rightArrowBtn = document.getElementById("rightArrowBtn");
+
+  var possibleCheckUpValues = ["Daily","Weekly","Monthly"];
+  var checkUpIndex = 0;
 
   nextBtn.addEventListener('click', function(){
-    window.location.href = "pages/reportCard/reportCard.html";
+    if( nameInput.value != null && nameInput.value != "" ){
+        //TODO: Save the user's name to the local database.
+        window.location.href = "pages/reportCard/reportCard.html";
+    }
+  });
+
+  leftArrowBtn.addEventListener('click', function(){
+    if( checkUpVal.innerHTML != "Daily" ){
+      //Change to the next greatest value.
+      this.checkUpIndex++;
+      checkUpVal.innerHTML = this.possibleCheckUpValues[this.checkUpIndex];
+    }
+  });
+
+  rightArrowBtn.addEventListener('click', function(){
+    if( checkUpVal.innerHTML != "Monthly" ){
+      //Change to the next lowest value.
+      this.checkUpIndex--;
+      checkUpVal.innerHTML = this.possibleCheckUpValues[this.checkUpIndex];
+    }
   });
 
 });
